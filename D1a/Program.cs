9046bc5d44374e58bea59b45357b1cef8e358973
Martin Jaros <jaros@arion.cz>
2018-12-01 19:47:55 +0100

    Day 1

---
+++ D1a/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace D1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int frequency = 0;
+            using (StreamReader input = File.OpenText("d:\\programming\\Advent of Code\\data\\D1a\\input.txt"))
+            {
+                string number = "";
+                while ((number = input.ReadLine()) != null)
+                {
+                    try
+                    {
+                        frequency += Convert.ToInt32(number);
+                    }
+                    catch { }
+                }
+            }
+            Console.WriteLine(frequency);
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ D1b/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace D1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("d:\\programming\\Advent of Code\\data\\D1a\\input.txt");
+            int frequency = 0;
+            List<int> history = new List<int>();
+
+            int i = 0;
+            history.Add(frequency);
+            while (true)
+            {
+                try
+                {
+                    frequency += Convert.ToInt32(input[i]);
+                    if (history.Contains(frequency))
+                        break;
+                    else
+                        history.Add(frequency);
+                }
+                catch { }
+
+                i++;
+                if (i >= input.Length)
+                    i = 0;
+            }
+
+            Console.WriteLine(frequency);
+            Console.ReadLine();
+        }
+
+    }
+
+}
