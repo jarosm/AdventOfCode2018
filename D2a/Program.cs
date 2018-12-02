30a53889b8616118ab2f027df24b56d23671488b
Martin Jaros <jaros@arion.cz>
2018-12-02 10:32:15 +0100

    Day 2

---
+++ D2a/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace D2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int twoTimes = 0, threeTimes = 0;
+            using (StreamReader input = File.OpenText("d:\\programming\\Advent of Code\\data\\D2a\\input.txt"))
+            {
+                string id = "";
+                while ((id = input.ReadLine()) != null)
+                {
+                    foreach (char c in id)
+                    {
+                        if (id.Count(x => x == c) == 2)
+                        {
+                            twoTimes++;
+                            break;
+                        }
+                    }
+                    foreach (char c in id)
+                    {
+                        if (id.Count(x => x == c) == 3)
+                        {
+                            threeTimes++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine((twoTimes * threeTimes).ToString());
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ D2b/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace D2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("d:\\programming\\Advent of Code\\data\\D2a\\input.txt");
+            int index1 = 0, index2 = 0;
+
+            int len = input.Length;
+            while (index1 < len)
+            {
+                index2 = index1 + 1;
+                Console.Write((index1 + 1).ToString() + "/" + len + "\r");
+
+                while (index2 < len)
+                {
+                    int differencies = 0;
+
+                    for (int i = 0; i < input[index1].Length; i++)
+                    {
+                        if (input[index1][i] != input[index2][i])
+                            differencies++;
+                    }
+
+                    if (differencies == 1)
+                        break;
+                    index2++;
+                }
+
+                if (index2 < len)
+                    break;
+                index1++;
+            }
+
+            Console.WriteLine("");
+            if ((index1 < len) && (index2 < len))
+            {
+                Console.WriteLine(input[index1]);
+                Console.WriteLine(input[index2]);
+            }
+            else
+                Console.WriteLine("error");
+            Console.ReadLine();
+        }
+
+    }
+
+}
