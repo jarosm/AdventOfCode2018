f709f65bee0d9aaea4d6676a92ead95399b9c48a
Martin Jaros <jaros@arion.cz>
2018-12-11 11:24:18 +0100

    Day 11

---
+++ D11a/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D11a
+{
+    class Program
+    {
+        static int gridSerialNumber = 8199; // input
+
+        static void Main(string[] args)
+        {
+            int[,] grid = new int[300, 300]; // rows, cols
+            int maxPower = int.MinValue, maxPowerX = -1, maxPowerY = -1;
+
+            for (int y = 0; y < 300; y++)
+            {
+                for (int x = 0; x < 300; x++)
+                {
+                    int powerLevel = (x + 1) + 10; // Find the fuel cell's rack ID, which is its X coordinate plus 10
+                    powerLevel *= y + 1; // Begin with a power level of the rack ID times the Y coordinate
+                    powerLevel += gridSerialNumber; // Increase the power level by the value of the grid serial number
+                    powerLevel *= (x + 1) + 10; // Set the power level to itself multiplied by the rack ID
+                    powerLevel = (powerLevel / 100) % 10; // Keep only the hundreds digit of the power level
+                    powerLevel -= 5; // Subtract 5 from the power level
+
+                    // add power level to all cells in 3x3 grid
+                    // save max value
+                    for (int j = y; (j >= 0) && (j >= y - 2); j--)
+                    {
+                        for (int i = x; (i >= 0) && (i >= x - 2); i--)
+                        {
+                            grid[j, i] += powerLevel;
+
+                            if ((i < 298) && (j < 298) && (grid[j, i] > maxPower))
+                            {
+                                maxPower = grid[j, i];
+                                maxPowerX = i + 1;
+                                maxPowerY = j + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("X,Y = " + maxPowerX.ToString() + "," + maxPowerY.ToString() + " ; " + maxPower.ToString());
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ D11b/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D11b
+{
+    class Program
+    {
+        static int gridSerialNumber = 8199; // input
+
+        static void Main(string[] args)
+        {
+            int[,] grid = new int[301, 301]; // rows, cols
+            int maxPower = int.MinValue, maxPowerX = -1, maxPowerY = -1, gridSize = 0;
+
+            for (int y = 1; y <= 300; y++)
+            {
+                for (int x = 1; x <= 300; x++)
+                {
+                    int powerLevel = x + 10; // Find the fuel cell's rack ID, which is its X coordinate plus 10
+                    powerLevel *= y; // Begin with a power level of the rack ID times the Y coordinate
+                    powerLevel += gridSerialNumber; // Increase the power level by the value of the grid serial number
+                    powerLevel *= x + 10; // Set the power level to itself multiplied by the rack ID
+                    powerLevel = (powerLevel / 100) % 10; // Keep only the hundreds digit of the power level
+                    powerLevel -= 5; // Subtract 5 from the power level
+
+                    // Partial sums -> https://en.wikipedia.org/wiki/Summed-area_table
+                    grid[y, x] = powerLevel + grid[y - 1, x] + grid[y, x - 1] - grid[y - 1, x - 1];
+                }
+            }
+
+            for (int gSize = 1; gSize <= 300; gSize++)
+            {
+                for (int y = gSize; y <= 300; y++)
+                {
+                    for (int x = gSize; x <= 300; x++)
+                    {
+                        int totalPower = grid[y, x] - grid[y - gSize, x] - grid[y, x - gSize] + grid[y - gSize, x - gSize];
+                        if (totalPower > maxPower)
+                        {
+                            maxPower = totalPower;
+                            maxPowerX = x;
+                            maxPowerY = y;
+                            gridSize = gSize;
+                        }
+                    }
+                }
+            }
+            maxPowerX += -gridSize + 1;
+            maxPowerY += -gridSize + 1;
+
+            Console.WriteLine("X,Y = " + maxPowerX.ToString() + "," + maxPowerY.ToString() + "," + gridSize.ToString());
+            Console.ReadLine();
+        }
+
+    }
+
+}
