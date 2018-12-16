d113598290a97de580cb9efe26e067d776155fe8
Martin Jaros <jaros@arion.cz>
2018-12-16 13:35:45 +0100

    Day 14

---
+++ D14a/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D14a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 323081;
+
+            List<int> recipes = new List<int>();
+            recipes.Add(3);
+            recipes.Add(7);
+
+            int elf1 = 0, elf2 = 1;
+
+            while (recipes.Count < input + 10)
+            {
+                int sum = recipes[elf1] + recipes[elf2];
+                if (sum >= 10)
+                    recipes.Add(1);
+                recipes.Add(sum % 10);
+
+                elf1 = (elf1 + 1 + recipes[elf1]) % recipes.Count;
+                elf2 = (elf2 + 1 + recipes[elf2]) % recipes.Count;
+            }
+
+            for (int i = input; i < input + 10; i++)
+            {
+                Console.Write(recipes[i].ToString());
+            }
+            
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ D14b/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D14b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 3, 2, 3, 0, 8, 1 };
+
+            List<int> recipes = new List<int>();
+            recipes.Add(3);
+            recipes.Add(7);
+
+            int elf1 = 0, elf2 = 1, index = 0, positionToCheck = 0;
+            bool found = false;
+
+            while (!found)
+            {
+                int sum = recipes[elf1] + recipes[elf2];
+                if (sum >= 10)
+                    recipes.Add(1);
+                recipes.Add(sum % 10);
+
+                elf1 = (elf1 + 1 + recipes[elf1]) % recipes.Count;
+                elf2 = (elf2 + 1 + recipes[elf2]) % recipes.Count;
+
+                while (index + positionToCheck < recipes.Count)
+                {
+                    if (input[positionToCheck] == recipes[index + positionToCheck])
+                    {
+                        if (positionToCheck == input.Length - 1)
+                        {
+                            found = true;
+                            break;
+                        }
+                        positionToCheck++;
+                    }
+                    else
+                    {
+                        positionToCheck = 0;
+                        index++;
+                    }
+                }
+            }
+
+            Console.WriteLine(index.ToString());
+            Console.ReadLine();
+        }
+
+    }
+
+}
